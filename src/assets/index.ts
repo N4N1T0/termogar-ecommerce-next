2473bf302406c9846e18502bfa552e88e8ecbd6b
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-11-07 14:17:03 +0100

    feat: Add assets

---
+++ src/assets/index.ts
@@ -0,0 +1,31 @@
+import Brand1 from './images/brand-1.png'
+import Brand2 from './images/brand-2.png'
+import Brand3 from './images/brand-3.png'
+import Brand4 from './images/brand-4.png'
+import Brand5 from './images/brand-5.png'
+import Brand6 from './images/brand-6.png'
+import Brand7 from './images/brand-7.png'
+import Brand8 from './images/brand-8.png'
+import Brand9 from './images/brand-9.png'
+import Brand10 from './images/brand-10.png'
+import Brand11 from './images/brand-11.png'
+import Brand12 from './images/brand-12.png'
+
+// * PLACEHOLDERS
+import PlaceholderSquare from './images/about-banner.png'
+
+export {
+  Brand1,
+  Brand2,
+  Brand3,
+  Brand4,
+  Brand5,
+  Brand6,
+  Brand7,
+  Brand8,
+  Brand9,
+  Brand10,
+  Brand11,
+  Brand12,
+  PlaceholderSquare
+}
