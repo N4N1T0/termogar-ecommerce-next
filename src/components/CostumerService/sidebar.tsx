44a70561c11b42e57b18b443e1756618499c3825
n4n1t0 <you@example.comadrian.alvarezalonso1991@gmail.com>
2024-11-20 16:05:03 +0100

    fix #3 : Add full height and gruop then

--- src/components/CostumerService/sidebar.tsx
+++ src/components/CostumerService/sidebar.tsx
@@ -9,7 +9,8 @@ import {
 import { sanityClientRead } from '@/sanity/lib/client'
 import { GET_COSTUMER_SERVICES_SIDEBAR_MENU } from '@/sanity/lib/queries'
 
-const MATCH_URL = 'servicio-de-atencion-al-cliente'
+const MATCH_URL_SERVICE = 'servicio-de-atencion-al-cliente'
+const MATCH_URL_PROFILE = 'mi-cuenta'
 
 const CostumerServiceSidebar = async ({
   slug
@@ -21,31 +22,57 @@ const CostumerServiceSidebar = async ({
   )
 
   const costumerServicesPages = searchedPages?.filter((page) => {
-    return page.link?.split('/').includes(MATCH_URL)
+    return page.link?.split('/').includes(MATCH_URL_SERVICE)
   })
 
-  const notCostumerServicesPages = searchedPages?.filter((page) => {
-    return !page.link?.split('/').includes(MATCH_URL)
+  const profileServicesPages = searchedPages?.filter((page) => {
+    return page.link?.split('/').includes(MATCH_URL_PROFILE)
+  })
+
+  const otherPages = searchedPages?.filter((page) => {
+    return (
+      !page.link?.split('/').includes(MATCH_URL_SERVICE) &&
+      !page.link?.split('/').includes(MATCH_URL_PROFILE)
+    )
   })
 
   // TODO: GROUP ????
 
   return (
-    <aside className='sticky top-0 h-full w-64 text-balance border-r border-gray-200 bg-gray-100 p-4'>
-      <nav aria-label='Customer Service Navigation' className='overflow-y-auto'>
-        <h2 className='mb-4 text-lg font-semibold'>
+    <aside className='sticky top-0 h-screen w-72 overflow-y-scroll text-balance border-r border-gray-200 bg-gray-100 p-4'>
+      <nav aria-label='Customer Service Navigation'>
+        <h2 className='mb-4 text-xl font-semibold uppercase text-accent'>
           Servicio de Atencion al cliente
         </h2>
 
         <Accordion type='single' collapsible className='mb-4'>
+          <AccordionItem value='cliente'>
+            <AccordionTrigger className='text-lg font-bold'>
+              Cliente
+            </AccordionTrigger>
+            <AccordionContent>
+              <ul className='space-y-2'>
+                {costumerServicesPages?.map(({ id, slug: pageSlug, title }) => (
+                  <li key={id} className='cursor-pointer'>
+                    <Link
+                      href={`/servicio-al-cliente/${pageSlug}`}
+                      className={`hover-200 flex items-center text-base underline ${pageSlug === slug ? 'text-accent hover:text-gray-900' : 'text-gray-900 hover:text-accent'}`}
+                    >
+                      {title}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </AccordionContent>
+          </AccordionItem>
           <AccordionItem value='links-de-ayuda'>
             <AccordionTrigger className='text-lg font-bold'>
               Links de Ayuda
             </AccordionTrigger>
             <AccordionContent>
               <ul className='space-y-2'>
-                {costumerServicesPages?.map(({ id, slug: pageSlug, title }) => (
-                  <li key={id}>
+                {profileServicesPages?.map(({ id, slug: pageSlug, title }) => (
+                  <li key={id} className='cursor-pointer'>
                     <Link
                       href={`/servicio-al-cliente/${pageSlug}`}
                       className={`hover-200 flex items-center text-base underline ${pageSlug === slug ? 'text-accent hover:text-gray-900' : 'text-gray-900 hover:text-accent'}`}
@@ -59,7 +86,7 @@ const CostumerServiceSidebar = async ({
           </AccordionItem>
         </Accordion>
         <ul className='space-y-2'>
-          {notCostumerServicesPages?.map(({ id, slug: pageSlug, title }) => (
+          {otherPages?.map(({ id, slug: pageSlug, title }) => (
             <li key={id}>
               <Link
                 href={`/servicio-al-cliente/${pageSlug}`}
