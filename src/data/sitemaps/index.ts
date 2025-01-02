68fe8cb9f7d78dda77c0a436cd3f1e837eac365e
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-01-02 12:00:34 +0100

    feat: Add sitemap data and implement redirects in middleware

---
+++ src/data/sitemaps/index.ts
@@ -0,0 +1,6 @@
+import etiquetas from './etiquetas.json'
+import products from './products.json'
+import posts from './posts.json'
+import raiola from './raiola.json'
+
+export { etiquetas, products, posts, raiola }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,37 @@
 import { auth } from '@/lib/auth'
 import { NextResponse } from 'next/server'
 
+// * REDIRECTS
+import { etiquetas, posts, products, raiola } from './data/sitemaps'
+
+// Example redirects array
+const redirects = [...etiquetas, ...posts, ...products, ...raiola]
+
 const protectedAuthRoutes = ['login', 'signup', 'reset-password']
 const protectedRoutes = ['perfil']
 
 export default auth((req) => {
-  const { pathname } = req.nextUrl
+  const { pathname, origin } = req.nextUrl
   const user = req.auth?.user
 
+  // Handle redirects
+  const redirectRule = redirects.find(
+    (rule) => rule.source === `${origin}${pathname}`
+  )
+  if (redirectRule) {
+    const statusCode = redirectRule.permanent ? 308 : 307
+    return NextResponse.redirect(new URL(redirectRule.destination), statusCode)
+  }
+
+  // Redirect authenticated users trying to access protected auth routes
   if (user && protectedAuthRoutes.includes(pathname.split('/')[1])) {
-    // Redirect authenticated users trying to access protected auth routes
-    const newUrl = new URL('/', req.nextUrl.origin)
+    const newUrl = new URL('/', origin)
     return NextResponse.redirect(newUrl)
   }
 
+  // Redirect unauthenticated users trying to access protected routes
   if (user === undefined && protectedRoutes.includes(pathname.split('/')[1])) {
-    // Redirect unauthenticated users trying to access protected routes
-    const newUrl = new URL('/', req.nextUrl.origin)
+    const newUrl = new URL('/', origin)
     return NextResponse.redirect(newUrl)
   }
 
