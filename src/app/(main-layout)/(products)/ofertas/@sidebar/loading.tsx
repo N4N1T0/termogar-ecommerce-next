e41311a8939a88cbdc2da9f22e4cbe7f2e6cdb5f
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-01-09 23:11:27 +0100

    feat: Add some new UI loading

--- src/app/(main-layout)/(products)/ofertas/@sidebar/loading.tsx
+++ src/app/(main-layout)/(products)/ofertas/@sidebar/loading.tsx
@@ -3,9 +3,14 @@ import React from 'react'
 const SidebarLoading = () => {
   return (
     <aside className='sticky top-0 m-4 hidden h-screen w-72 animate-pulse divide-y-[1px] overflow-y-auto text-balance bg-white px-4 pt-10 md:block'>
-      <div className='my-3 h-6 w-2/4 rounded bg-gray-200'></div>
-      <div className='my-3 h-20 w-full rounded bg-gray-200'></div>
-      <div className='my-3 h-20 w-full rounded bg-gray-200'></div>
+      {Array(8)
+        .fill('SidebarCategories')
+        .map((item, index) => (
+          <div
+            key={`${item}-${index}`}
+            className='my-3 h-20 w-full rounded bg-gray-200'
+          />
+        ))}
     </aside>
   )
 }
