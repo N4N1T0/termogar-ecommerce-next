b64918b3f2800cdcdaaa808e54a9c0056623ece6
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-03-28 09:27:56 +0100

    fix(tailwind-wrapper): replace icon components with SVGs for improved performance and customization

--- src/emails/utils/tailwind-wrapper.tsx
+++ src/emails/utils/tailwind-wrapper.tsx
@@ -1,4 +1,3 @@
-import { Facebook, Instagram, Youtube } from 'lucide-react'
 import {
   Body,
   Container,
@@ -134,7 +133,20 @@ const TailwindWrapper = ({
                         target='_blank'
                         className='mr-2 flex aspect-square size-7 items-center justify-center rounded-full bg-accent'
                       >
-                        <Facebook className='text-gray-50' size={20} />
+                        <svg
+                          xmlns='http://www.w3.org/2000/svg'
+                          width='20'
+                          height='20'
+                          viewBox='0 0 24 24'
+                          fill='none'
+                          stroke='#ffffff'
+                          stroke-width='2'
+                          stroke-linecap='round'
+                          stroke-linejoin='round'
+                          className='lucide lucide-facebook-icon lucide-facebook'
+                        >
+                          <path d='M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z' />
+                        </svg>
                       </Link>
                     </Column>
                     <Column>
@@ -142,7 +154,29 @@ const TailwindWrapper = ({
                         href='https://www.instagram.com/termogar.es/'
                         className='mr-2 flex aspect-square size-7 items-center justify-center rounded-full bg-accent'
                       >
-                        <Instagram className='text-gray-50' size={20} />
+                        <svg
+                          xmlns='http://www.w3.org/2000/svg'
+                          width='20'
+                          height='20'
+                          viewBox='0 0 24 24'
+                          fill='none'
+                          stroke='#ffffff'
+                          stroke-width='2'
+                          stroke-linecap='round'
+                          stroke-linejoin='round'
+                          className='lucide lucide-instagram-icon lucide-instagram'
+                        >
+                          <rect
+                            width='20'
+                            height='20'
+                            x='2'
+                            y='2'
+                            rx='5'
+                            ry='5'
+                          />
+                          <path d='M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z' />
+                          <line x1='17.5' x2='17.51' y1='6.5' y2='6.5' />
+                        </svg>
                       </Link>
                     </Column>
                     <Column>
@@ -150,7 +184,21 @@ const TailwindWrapper = ({
                         href='https://www.youtube.com/channel/UC2bX_gn3IX27PP2fyDpbhbg'
                         className='mr-2 flex aspect-square size-7 items-center justify-center rounded-full bg-accent'
                       >
-                        <Youtube className='text-gray-50' size={20} />
+                        <svg
+                          xmlns='http://www.w3.org/2000/svg'
+                          width='20'
+                          height='20'
+                          viewBox='0 0 24 24'
+                          fill='none'
+                          stroke='#ffffff'
+                          stroke-width='2'
+                          stroke-linecap='round'
+                          stroke-linejoin='round'
+                          className='lucide lucide-youtube-icon lucide-youtube'
+                        >
+                          <path d='M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17' />
+                          <path d='m10 15 5-3-5-3z' />
+                        </svg>
                       </Link>
                     </Column>
                   </Row>
